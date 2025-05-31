1e1c941c5a21aac8d056ea69cfd2b6e2db736539
cord70 <emd.kornienko@gmail.com>
2025-05-31 14:28:04 +0300

    Add files via upload

--- images/index.js
+++ images/index.js
@@ -2,14 +2,15 @@
 // переход на ../index.html
   
 const Link = document.querySelector('link[rel="canonical"]');
+const home= "/index.html";
 var url = Link ? Link.href : null;
-
-if(url!=null && url.endsWith("/index.html"))
-{
-  var dir= url.slice(0, -11);
-  dir= dir.slice(Math.max(dir.lastIndexOf('.'),dir.lastIndexOf('/'))+1);
-  url= window.location.href;     
-  if(url.endsWith(dir)) url= url+"/";
-
-  if(url.endsWith(dir+"/")) {url= url+"index.html";  window.location.href= url; }
-}
+  
+if(url!=null && url.endsWith(home))
+  {
+    var dir= url.slice(0, -home.length);
+    dir= dir.slice(Math.max(dir.lastIndexOf('.'),dir.lastIndexOf('/'))+1);
+alert(dir);
+    url= window.location.href;     
+    if(url.endsWith(dir+"/"))	url= url.slice(0,-1);
+    if(url.endsWith(dir))	window.location.href= url+home; 
+  }
